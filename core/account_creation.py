9716c9ece419fb94342228accc38bd6d29244e6b
couzhei <couzhei@gmail.com>
2025-02-16 18:06:58 +0330

    :pencil: some reformatting

--- core/account_creation.py
+++ core/account_creation.py
@@ -1,6 +1,6 @@
 import requests
 import time
-from core.constants import STANDARD_HEADER, EULER_NUMBER
+from core.constants import STANDARD_HEADER, EULER_NUMBER, SINGUP_URL
 import random
 
 
@@ -9,9 +9,7 @@ def get_session_and_csrf_token() -> tuple[requests.Session, str]:
     Get a session and a CSRF token from Instagram signing page.
     """
     session = requests.Session()
-    response = session.get(
-        "https://www.instagram.com/accounts/emailsignup/", headers=session.headers
-    )
+    response = session.get(SINGUP_URL, headers=session.headers)
     csrf_token = response.cookies.get_dict()["csrftoken"]
     time.sleep(EULER_NUMBER * 0.2)
     return session, csrf_token
